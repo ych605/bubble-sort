286d88cf7d980836d0f3992bfb8eebd3b67b6a84
Jacky yu <ych605@gmail.com>
2020-09-14 22:38:36 +0800

    update to prevent mutation of the original list

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 export const bubbleSort = list => {
-  for (let i = 0; i < list.length - 1; i++) {
-    for (let j = 0; j < list.length - 1 - i; j++) {
-      if (list[j] > list[j+1]) {
-        [ list[j], list[j+1] ] = [ list[j+1], list[j] ] // swap ajacent elements
+  const newList = [...list];
+  for (let i = 0; i < newList.length - 1; i++) {
+    for (let j = 0; j < newList.length - 1 - i; j++) {
+      if (newList[j] > newList[j+1]) {
+        [ newList[j], newList[j+1] ] = [ newList[j+1], newList[j] ]
       }
     }
   }
-  return list
+  return newList
 }
